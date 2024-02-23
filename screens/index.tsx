3d7b10565e3d082dd1c62e6b1daa05a710eeb41c
NathanJargon <metamorphiaSH@gmail.com>
2024-02-23 09:40:11 +0800

    2024

--- screens/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as HomeScreen } from './LoginHomeScreen';
-export { default as LoginScreen } from './LoginScreen';
-export { default as RegisterScreen } from './RegisterScreen';
-export { default as ForgotPasswordScreen } from './ForgotPasswordScreen';
-export { default as Dashboard } from './Dashboard';
